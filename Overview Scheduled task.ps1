3f2af11c88e1f134879be270471b2cf07c2014c9
DigitalFunwork <75845785+DigitalFunwork@users.noreply.github.com>
2023-02-16 08:25:17 +0100

    Add files via upload

---
+++ Overview Scheduled task.ps1
@@ -0,0 +1,18 @@
+﻿
+$outcsv = "c:\temp\tasks_$env:computername.csv"
+
+$taskPath = "\coda\*"
+
+Get-ScheduledTask -TaskPath $taskPath |
+    ForEach-Object { [pscustomobject]@{
+     Name = $_.TaskName
+     Path = $_.TaskPath
+     LastResult = $(($_ | Get-ScheduledTaskInfo).LastTaskResult)
+     NextRun = $(($_ | Get-ScheduledTaskInfo).NextRunTime)
+     Status = $_.State
+     Command = $_.Actions.execute
+     Arguments = $_.Actions.Arguments }
+     } |  Export-Csv -Path $outcsv -NoTypeInformation
+
+gc $outcsv
+ls $outcsv
